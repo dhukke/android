cfc99fa91495ccc4d10034916a1a59beb4cce241
Jose Garcia Netto <jnetto@scopus.com.br>
2016-09-27 12:06:57 -0300

    Added project my browser

---
+++ MyBrowser/app/src/main/java/com/example/jnetto/mybrowser/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.jnetto.mybrowser;
+
+import android.graphics.Bitmap;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private WebView webView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        String url = "http://" + editText.getText().toString();
+        webView.loadUrl(url);
+    }
+}
