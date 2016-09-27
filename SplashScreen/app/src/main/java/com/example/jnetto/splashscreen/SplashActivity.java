c1c5afad565cd69b87989c1335e5793a0486aac6
Jose Garcia Netto <jnetto@scopus.com.br>
2016-09-27 10:04:55 -0300

    added splash screen project

---
+++ SplashScreen/app/src/main/java/com/example/jnetto/splashscreen/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.jnetto.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
